501b23784f0c7641eb780d8cc8213479081aa7b0
nsx2128 <89470761+nsx2128@users.noreply.github.com>
2021-08-24 11:26:09 -0500

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -1,7 +1,10 @@
 import java.util.regex.Pattern
 import java.util.regex.Matcher
+import groovy.time.TimeCategory 
+import groovy.time.TimeDuration
 
-def Integer simpleMatch(String document, String matchValue) {
+def String simpleMatch(String document, String matchValue) {
+    Date start = new Date()
     Integer relevancy = 0
     Integer matchSize = matchValue.size() - 1
     for(i = 0; i<document.size(); i++) {
@@ -9,19 +12,29 @@ def Integer simpleMatch(String document, String matchValue) {
             relevancy += 1
         }
     }
-    return relevancy
+    Date stop = new Date()
+    TimeDuration td = TimeCategory.minus( stop, start )
+    return "Elapsed time: $td, $relevancy matches"
 }
 
-def Integer regexMatch(String document, String regexValue) {
+def String regexMatch(String document, String regexValue) {
+    Date start = new Date()
     Integer relevancy = 0
     def pattern = ~regexValue
     def matcher = document =~ pattern
     matcher.find()
-    return matcher.size()
+    def results = matcher.size()
+    Date stop = new Date()
+    TimeDuration td = TimeCategory.minus( stop, start )
+    return "Elapsed time: $td, $results matches"
 }
 
 /*
 def preProcessMatch(String document, String matchValue) {
+    Date start = new Date()
     document = document.toLowerCase().replaceAll("\\s", "")
+    Date stop = new Date()
+    TimeDuration td = TimeCategory.minus( stop, start )
+    return "Elapsed time: $td, $results matches"
 }
 */
